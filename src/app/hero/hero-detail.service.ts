e8779b8e54d91a877a60824d0f0c1770f039e343
Julien HENRY <julien.henry@sonarsource.com>
2019-02-11 17:42:35 +0100

    Add one issue

--- src/app/hero/hero-detail.service.ts
+++ src/app/hero/hero-detail.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Hero } from '../model/hero';
-import { HeroService } from '../model/hero.service';
+import { Hero } from "../model/hero";
+import { HeroService } from "../model/hero.service";
 
 @Injectable()
 export class HeroDetailService {
-  constructor(private heroService: HeroService) {  }
+  constructor(private heroService: HeroService) {}
 
   // Returns a clone which caller may modify safely
   getHero(id: number | string): Promise<Hero> {
-    if (typeof id === 'string') {
+    if (typeof id === "string") {
       id = parseInt(id as string, 10);
+    } else if (typeof id === "string") {
+      id = id as number;
     }
     return this.heroService.getHero(id).then(hero => {
       return hero ? Object.assign({}, hero) : null; // clone or null
