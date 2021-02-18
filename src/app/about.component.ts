99b466d740df27bec8b96bdacca384ae01dddde4
Gregoire Aubert <gregoire.aubert@sonarsource.com>
2021-02-18 14:49:12 +0000

    Edit file without adding new issues

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
   template: `
   <h2 highlight="skyblue">About</h2>
   <twain-quote></twain-quote>
+  <p>This should not add any issues</p>
   <p>All about this sample</p>`
 })
 export class AboutComponent { }
